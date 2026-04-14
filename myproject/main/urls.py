09b16e460539cb9d538b17bf283ec99341af0043
Gary-Jader7709 <gary92123122@gmail.com>
2026-04-14 22:46:27 +0800

    0414

--- myproject/main/urls.py
+++ myproject/main/urls.py
@@ -2,5 +2,6 @@
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
+    path('course/<int:course_id>/', views.course_detail, name='course_detail'),
 ]
--- myproject/main/views.py
+++ myproject/main/views.py
@@ -1,6 +1,10 @@
-﻿from django.shortcuts import render
+﻿from django.shortcuts import render, get_object_or_404
 from .models import Course
 
 def home(request):
     courses = Course.objects.all()
     return render(request, 'main/home.html', {'courses': courses})
+
+def course_detail(request, course_id):
+    course = get_object_or_404(Course, id=course_id)
+    return render(request, 'main/course_detail.html', {'course': course})
